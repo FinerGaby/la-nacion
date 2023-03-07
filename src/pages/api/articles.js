7f92d3f03f0bc6580c6d3018d9d2315d98d0c949
Gabi <figueiras.trabajo@gmail.com>
2023-03-07 02:21:41 -0300

    env

--- src/pages/api/articles.js
+++ src/pages/api/articles.js
@@ -1,17 +1,16 @@
+import { API_URL } from "../../../config";
+
 export default async function handler(_req, res) {
   try {
-    const fetching = await fetch(
-      "https://jrt2bb3b2nlkw5ozvfcld62wbe0pnifh.lambda-url.us-east-1.on.aws",
-      {
-        method: "GET",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const fetching = await fetch(API_URL, {
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     let data = await fetching.json();
     res.json(data?.articles);
   } catch (e) {
